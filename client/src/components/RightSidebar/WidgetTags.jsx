0db953f4dfbd00bedf8da4023af47702b2f6b513
Anuja Sunil Kokate <anujakokate18@gmail.com>
2022-07-28 21:43:24 +0530

    Tags added

--- client/src/components/RightSidebar/WidgetTags.jsx
+++ client/src/components/RightSidebar/WidgetTags.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
 const WidgetTags = () => {
+    const tags=['c','css','express','firebase','html','java','mern','mongodb','mysql','nextjs','nodejs','php','python','reactjs']
   return (
-    <div>WidgetTags</div>
+    <div className='widget-tags'>
+        <h4>Watched tags</h4>
+        <div className="widget-tags-div">
+            {
+                tags.map((tag)=>(
+                    <p key={tag}>{tag}</p>
+                ))
+            }
+        </div>
+    </div>
   )
 }
 
