e0bd99a61d8f2a721e1c546dc4f5ba91c19e5eda
Anuja Sunil Kokate <anujakokate18@gmail.com>
2022-08-03 22:34:46 +0530

    tags added

--- client/src/components/HomeMainbar/Questions.jsx
+++ client/src/components/HomeMainbar/Questions.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Questions = ({question}) => {
   return (
@@ -6,6 +7,20 @@ const Questions = ({question}) => {
         <div className="display-votes-container">
             <p>{question.votes}</p>
             <p>votes</p>
+        </div>
+        <div className="display-votes-container">
+            <p>{question.noOfAnswers}</p>
+            <p>answers</p>
+        </div>
+        <div className="display-questions-details">
+          <Link to={`/Questions/${question.id}`} className='question-title-link'>{question.QuestionTitle}</Link>
+          <div className="display-tags-time">
+            {
+              question.questionTags.map((tag)=>(
+                <p key={tag}>{tag}</p>
+              ))
+            }
+          </div>
         </div>
     </div>
   )
