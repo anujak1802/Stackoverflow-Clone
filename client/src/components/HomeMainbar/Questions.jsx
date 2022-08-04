e1b2826e37999309c759183198bca4a7a91252a0
Anuja Sunil Kokate <anujakokate18@gmail.com>
2022-08-04 22:36:46 +0530

    CSS for questions done

--- client/src/components/HomeMainbar/Questions.jsx
+++ client/src/components/HomeMainbar/Questions.jsx
@@ -3,26 +3,28 @@ import { Link } from 'react-router-dom'
 
 const Questions = ({question}) => {
   return (
-    <div className='display-ans-container'>
-        <div className="display-votes-container">
+    <div className='display-question-container'>
+        <div className="display-votes-ans">
             <p>{question.votes}</p>
             <p>votes</p>
         </div>
-        <div className="display-votes-container">
+        <div className="display-votes-ans">
             <p>{question.noOfAnswers}</p>
             <p>answers</p>
         </div>
         <div className="display-questions-details">
           <Link to={`/Questions/${question.id}`} className='question-title-link'>{question.QuestionTitle}</Link>
           <div className="display-tags-time">
-            {
-              question.questionTags.map((tag)=>(
-                <p key={tag}>{tag}</p>
-              ))
-            }
-          </div>
-          <div className="display-time">
-            <p>asked {question.time} {question.userPosted}</p>
+            <div className="display-tags">
+              {
+                question.questionTags.map((tag)=>(
+                  <p key={tag}>{tag}</p>
+                ))
+              }
+            </div>
+            <p className="display-time">
+              asked {question.time} {question.userPosted}
+            </p>
           </div>
         </div>
     </div>
