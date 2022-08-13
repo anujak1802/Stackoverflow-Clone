54cdd98ca3b2462baaf205c02607edd2fcfd344e
Anuja Sunil Kokate <anujakokate18@gmail.com>
2022-08-13 19:52:01 +0530

    Display done

--- client/src/pages/Questions/DisplayAnswer.jsx
+++ client/src/pages/Questions/DisplayAnswer.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Avatar from '../../components/Avatar/Avatar'
+import './Questions.css'
 
-const DisplayAnswer = () => {
+const DisplayAnswer = ({ question }) => {
   return (
-    <div>DisplayAnswer</div>
+    <div>
+      {
+        question.answer.map((ans) => (
+          <div className='display-ans' key={ans._id}>
+            <p>{ans.answerBody}</p>
+            <div className="question-actions-user">
+              <div>
+                <button type='button'>Share</button>
+                <button type='button'>Delete</button>
+              </div>
+              <div>
+                <p>answered {ans.answeredOn}</p>
+                <Link to={`/User/${question.userId}`} className='user-link' style={{ color: '#008d68' }}>
+                  <Avatar backgroundColor="green" px='8px' py='5px'>{ans.userAnswered.charAt(0).toUpperCase()}</Avatar>
+                  <div>
+                    {ans.userAnswered}
+                  </div>
+                </Link>
+              </div>
+            </div>
+
+          </div>
+
+        ))
+      }
+    </div>
   )
 }
 
--- client/src/pages/Questions/QuestionDetails.jsx
+++ client/src/pages/Questions/QuestionDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams,Link } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import upvote from '../../assests/caret-up-solid.svg'
 import downvote from '../../assests/caret-down-solid.svg'
 import './Questions.css'
@@ -74,39 +74,42 @@ const QuestionDetails = () => {
                                         <h1>{question.QuestionTitle}</h1>
                                         <div className="question-details-container-2">
                                             <div className="question-votes">
-                                                <img src={upvote} alt="" width="18" />
+                                                <img src={upvote} alt="" width="18" className='votes-icon' />
                                                 <p>{question.upVote - question.downVote}</p>
-                                                <img src={downvote} alt="" width="18"/>
+                                                <img src={downvote} alt="" width="18" className='votes-icon' />
                                             </div>
-                                            <div style={{width:"100%"}}>
+                                            <div style={{ width: "100%" }}>
                                                 <div className="question-body">
                                                     {question.QuestionBody}
                                                 </div>
                                                 <div className="question-details-tags">
                                                     {
-                                                        question.questionTags.map((tag)=>(
+                                                        question.questionTags.map((tag) => (
                                                             <p key={tag}>{tag}</p>
                                                         ))
                                                     }
                                                 </div>
-                                                <div className="question-action-user">
-                                                    <button type='button'>Share</button>
-                                                    <button type='button'>Delete</button>
+                                                <div className="question-actions-user">
+                                                    <div>
+                                                        <button type='button'>Share</button>
+                                                        <button type='button'>Delete</button>
+                                                    </div>
+                                                    <div>
+                                                        <p>asked {question.time}</p>
+                                                        <Link to={`/User/${question.userId}`} className='user-link' style={{ color: '#008d68' }}>
+                                                            <Avatar backgroundColor="orange" px='8px' py='5px'>{question.userPosted.charAt(0).toUpperCase()}</Avatar>
+                                                            <div>
+                                                                {question.userPosted}
+                                                            </div>
+                                                        </Link>
+                                                    </div>
                                                 </div>
-                                                <div>
-                                                    <p>asked {question.time}</p>
-                                                    <Link to={`/User/${question.userId}`} className='user-link' style={{color:'#008d68'}}>
-                                                        <Avatar backgroundColor="orange" px='8px' py='5px'>{question.userPosted.charAt(0).toUpperCase()}</Avatar>
-                                                        <div>
-                                                            {question.userPosted}
-                                                        </div>
-                                                    </Link>
-                                                </div>
+
                                             </div>
                                         </div>
                                     </section>
                                     {
-                                        question.noOfAnswers !==0 && (
+                                        question.noOfAnswers !== 0 && (
                                             <section>
                                                 <h3>{question.noOfAnswers} answers</h3>
                                                 <DisplayAnswer key={question._id} question={question}></DisplayAnswer>
@@ -117,16 +120,16 @@ const QuestionDetails = () => {
                                         <h3>Your Answer</h3>
                                         <form >
                                             <textarea name="" id="" cols="30" rows="10"></textarea><br />
-                                            <input type="submit" className='post-ans-btn' value="Post you Answer" /> 
+                                            <input type="submit" className='post-ans-btn' value="Post you Answer" />
                                         </form>
                                         <p>
                                             Browse other questions
                                             {
-                                                question.questionTags.map((tag)=>(
+                                                question.questionTags.map((tag) => (
                                                     <Link to='/Tags' key={tag} className='ans-tags'>{tag}</Link>
                                                 ))
-                                            } or 
-                                            <Link to='/AskQuestion' style={{textDecoration:'none', color:'#009dff'}}> ask your own question</Link>
+                                            } or
+                                            <Link to='/AskQuestion' style={{ textDecoration: 'none', color: '#009dff' }}> ask your own question</Link>
                                         </p>
                                     </section>
                                 </div>
