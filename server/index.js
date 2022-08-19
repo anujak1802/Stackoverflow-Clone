99c84e24e5091629a388476f514acbde94c0d610
Anuja Sunil Kokate <anujakokate18@gmail.com>
2022-08-19 23:31:04 +0530

    PORT running

--- server/index.js
+++ server/index.js
@@ -2,14 +2,20 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
-const app=express();
+const app = express();
 
-app.use(express.json({limit:"30mb",exteneded:true}))
-app.use(express.urlencoded({limit:"30mb",exteneded:true}))
+app.use(express.json({ limit: "30mb", exteneded: true }))
+app.use(express.urlencoded({ limit: "30mb", exteneded: true }))
 app.use(cors());
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("This is TeachMeHow website")
 })
 
-const PORT=process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
+
+const CONNECTION_URL = "mongodb+srv://anujak1809:nayanakokate19@teachmehow.z6wkeoy.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => { console.log(`server is running on port ${PORT}`) }))
+    .catch((err) => console.log(err.message))
